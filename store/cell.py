715567bae190e04297358658334c40a5bd0e2866
zkroliko <zbkrolikowski@gmail.com>
2017-03-22 21:51:30 +0100

    Make the clients not collide

--- store/cell.py
+++ store/cell.py
@@ -3,7 +3,6 @@ import random
 
 
 class Cell(Agent):
-
     def __init__(self, pos, model):
         super().__init__(pos, model)
         self.x, self.y = pos
@@ -13,10 +12,14 @@ class Cell(Agent):
         return self.model.grid.neighbor_iter((self.x, self.y), True)
 
     def step(self):
-        #for neighbor in self.neighbors:
+        # for neighbor in self.neighbors:
         self.advance()
 
     def advance(self):
-        self.x = self.x + random.randint(-1,1)
-        self.y = self.y + random.randint(-1,1)
-        self.model.grid.move_agent(self,((self.x+1)%self.model.width,(self.y+1)%self.model.height))
+        x = (self.x + random.randint(-1, 1)) % self.model.width
+        y = (self.y + random.randint(-1, 1)) % self.model.height
+        while (not self.model.grid.is_cell_empty((x, y))):
+            x = (self.x + random.randint(-1, 1)) % self.model.width
+            y = (self.y + random.randint(-1, 1)) % self.model.height
+        self.x, self.y = (x, y)
+        self.model.grid.move_agent(self, (self.x , self.y))
