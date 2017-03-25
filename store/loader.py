06cfe1cbd31fe2b66c39af57ecb968b245976242
zkroliko <zbkrolikowski@gmail.com>
2017-03-25 18:25:04 +0100

    Load maps from a .json file

---
+++ store/loader.py
@@ -0,0 +1,37 @@
+import json
+
+from store.entrance import Entrance
+from store.exit import Exit
+from store.shelf import Shelf
+
+
+class Loader:
+    def __init__(self, model, path):
+        self.model = model
+        self.path = path
+
+    def load_from_json(self):
+        with open(self.path) as file:
+            data = json.load(file)
+            self._load_entrances(data["entrances"])
+            self._load_exits(data["exits"])
+            self._load_shelves(data["shelves"])
+
+    def _load_entrances(self, list):
+        for e in list:
+            entrance = Entrance((e["x"],e["y"]), self.model)
+            self.model.grid.place_agent(entrance, (e["x"],e["y"]))
+            self.model.schedule.add(entrance)
+
+    def _load_exits(self, list):
+        for e in list:
+            exit = Exit((e["x"],e["y"]), self.model)
+            self.model.grid.place_agent(exit, (e["x"],e["y"]))
+            self.model.schedule.add(exit)
+
+    def _load_shelves(self, list):
+        for s in list:
+            exit = Shelf((s["x"],s["y"]), self.model)
+            self.model.grid.place_agent(exit, (s["x"],s["y"]))
+            self.model.schedule.add(exit)
+
--- store/model.py
+++ store/model.py
@@ -6,6 +6,7 @@ from mesa.space import Grid
 
 from store.entrance import Entrance
 from store.exit import Exit
+from store.loader import Loader
 from store.shelf import Shelf
 from store.client import Client
 
@@ -21,44 +22,12 @@ class Shop(Model):
         # state of all its neighbors -- before they've changed.
         self.schedule = SimultaneousActivation(self)
 
-        # Use a simple grid, where edges wrap around.
         self.height = height
         self.width = width
         self.grid = Grid(height, width, torus=False)
 
-        for i in range(10, height - 10):
-            cell = Exit((i, 0), self)
-            print((i, height))
-            self.grid.place_agent(cell, (i, 0))
-            self.schedule.add(cell)
-
-        for i in range(0, 20):
-            cell = Shelf((5, i + 5), self)
-            self.grid.place_agent(cell, (5, i + 5))
-            self.schedule.add(cell)
-
-        for i in range(0, 20):
-            cell = Shelf((15, i + 5), self)
-            self.grid.place_agent(cell, (15, i + 5))
-            self.schedule.add(cell)
-
-        for i in range(0, 20):
-            cell = Shelf((25, i + 5), self)
-            self.grid.place_agent(cell, (25, i + 5))
-            self.schedule.add(cell)
-
-        for i in range(0, height - 1):
-            cell = Entrance((i, height - 1), self)
-            self.grid.place_agent(cell, (i, height - 1))
-            self.schedule.add(cell)
-
-        # for i in range(0, 50):
-        #     x = random.randrange(width)
-        #     y = random.randrange(int(height / 5)) - int(height / 5)
-        #     cell = Client((x, y), self)
-        #     if (self.grid.is_cell_empty((x, y))):
-        #         self.grid.place_agent(cell, (x, y))
-        #     self.schedule.add(cell)
+        loader = Loader(self,"example.json")
+        loader.load_from_json()
 
         self.running = True
 
