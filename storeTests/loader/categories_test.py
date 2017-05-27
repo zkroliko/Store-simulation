935894aedfcdc72335a856dd69c1102ad70139f6
zkroliko <zbkrolikowski@gmail.com>
2017-05-27 11:54:07 +0200

    Add test for loading categories

---
+++ storeTests/loader/categories_test.py
@@ -0,0 +1,45 @@
+import json
+import unittest
+from time import sleep
+
+import matplotlib.pyplot as plt
+import numpy
+
+from store.builder import Builder
+
+
+class TestPlacesToBe(unittest.TestCase):
+    input_json = {
+        "categories": ["m", "n", "p", "s", "c", "k", "h", "b", "g", "a", "d", "f"],
+        "c_probabilities": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    }
+
+    def test_general(self):
+            builder = Builder(self.input_json)
+            probs = builder.get_category_probabilities()
+            names = numpy.arange(len(probs))
+
+            self.assertEqual(len(names),12)
+            self.assertEqual(len(names),len(probs))
+
+    def test_graph(self):
+
+            builder = Builder(self.input_json)
+
+            bar_width = 0.35
+
+            values = builder.get_category_probabilities()
+            index = numpy.arange(len(values))
+
+            x_labels = builder.get_categories()
+
+            fig, ax = plt.subplots()
+            plt.title("Probabilites of occurance of items for categories")
+
+            plt.bar(index, values, width=bar_width)
+            plt.xlabel('Categories')
+            plt.ylabel('Probabilities')
+            plt.xticks(index + bar_width / 2, x_labels)
+            ax.set_xticklabels(x_labels)
+
+            plt.show()
