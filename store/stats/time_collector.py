649e539d7be809247555cdca0273911f057fbb9b
zkroliko <zbkrolikowski@gmail.com>
2017-05-28 19:18:48 +0200

    Add plotting of histogram for both datasets

--- store/stats/time_collector.py
+++ store/stats/time_collector.py
@@ -15,7 +15,7 @@ class TimeCollector:
         return [t[0] for t in self.times]
 
     @property
-    def walking_times(self):
+    def active_times(self):
         return [t[1] for t in self.times]
 
     def commit_time(self, total_time, actions):
@@ -31,12 +31,38 @@ class TimeCollector:
         self.plot()
 
     def plot(self):
-        self._plot_data(self.total_times, "Total turns spent by actors in the simulation", 'Turns spent in the shop',
+        self._plot_together()
+        self._plot_single(self.total_times, "Total turns spent by actors in the simulation", 'Turns spent in the shop',
                         'Count')
-        self._plot_data(self.walking_times, "Total turns spent by actors walking in the simulation", 'Turns spent walking',
+        self._plot_single(self.active_times, "Turns spent by actors active in the simulation", 'Turns spent active',
                         'Count')
 
-    def _plot_data(self, data, description="", x_label="", y_label=""):
+    def _plot_together(self):
+        n_bins = 30
+        fig, axes = plt.subplots(nrows=3, ncols=1)
+        ax0, ax1, ax2 = axes.flatten()
+
+        x = (self.total_times,self.active_times)
+
+        colors = ['red', 'tan']
+        labels = ['total', 'active']
+        title = 'Turns spent by actors in the simulation'
+        ax0.hist(x, n_bins, histtype='bar', color=colors, label=labels)
+        ax0.legend(prop={'size': 10})
+        ax0.set_title(title)
+
+        ax1.hist(x, n_bins, histtype='bar', stacked=True, label=labels)
+        ax1.legend(prop={'size': 10})
+        ax1.set_title(title)
+
+        ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False, label=labels)
+        ax2.legend(prop={'size': 10})
+        ax2.set_title(title)
+
+        fig.tight_layout()
+        plt.show()
+
+    def _plot_single(self, data, description="", x_label="", y_label=""):
         plot = plt.plot()
         plt.hist(data, 20, linewidth=1, edgecolor='black')
         plt.title(description)
@@ -50,9 +76,9 @@ class TimeCollector:
                + "Median turns in shop: {}\n".format(numpy.median(self.total_times)) \
                + "Standard deviation for turns in shop: {}\n".format(numpy.std(self.total_times)) \
                + "WALKING TIME:\n" \
-               + "Average turns walking in shop: {}\n".format(numpy.mean(self.walking_times)) \
-               + "Median turns walking in shop: {}\n".format(numpy.median(self.walking_times)) \
-               + "Standard deviation for turns walking in shop: {}".format(numpy.std(self.walking_times))
+               + "Average turns walking in shop: {}\n".format(numpy.mean(self.active_times)) \
+               + "Median turns walking in shop: {}\n".format(numpy.median(self.active_times)) \
+               + "Standard deviation for turns walking in shop: {}".format(numpy.std(self.active_times))
 
     def print_report(self):
         print(self.report())
